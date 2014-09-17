96bc2ba697b2d752eb0102bf0691211e1a67e753
Dave O <davidobrite@gmail.com>
2014-09-17 05:59:13 -0700

    big refactor

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,14 @@ impl Point {
     }
 }
 
-enum Contains {
-    DoesContain,
-    DoesNotContain
-}
-
 struct Bound {
     min: Point,
     max: Point
+}
+
+enum Contains {
+    DoesContain,
+    DoesNotContain
 }
 
 impl Bound {
@@ -46,32 +46,43 @@ impl Bound {
     }
 }
 
-
-
-fn render(con: &mut Console, c_point: Point, d_point: Point) {
-    con.clear();
-    con.put_char(c_point.x, c_point.y, '@', background_flag::Set);
-    con.put_char(d_point.x, d_point.y, 'd', background_flag::Set);
-    con.flush();
+struct Game {
+    exit:           bool,
+    window_bounds: Bound
 }
 
-fn main() {
-    let window_bounds = Bound { min: Point { x: 0, y: 0 }, max: Point { x: 79, y: 49 } };
-    let mut con = Console::init_root(window_bounds.max.x, window_bounds.max.y, "libtcod Rust tutorial", false);
+struct Character {
+    position:     Point,
+    display_char: char
+}
 
-    let mut exit = false;
+impl Character {
+    fn new(x: int, y: int, dc: char) -> Character {
+        Character { position: Point { x: x, y: y }, display_char: dc }
+    }
+}
 
-    let mut char_point = Point { x: 40i, y: 25i };
-    let mut dog_point  = Point { x: 10i, y: 10i };
+struct NPC {
+    position:     Point,
+    display_char: char
+}
 
-    render(&mut con, char_point, dog_point);
+impl NPC {
+    fn new(x: int, y: int, dc: char) -> NPC {
+        NPC { position: Point { x: x, y: y }, display_char: dc }
+    }
+}
 
-    while !(Console::window_closed() || exit) {
-        let keypress = con.wait_for_keypress(true);
+trait Updates {
+    fn update(&mut self, tcod::KeyState, Game);
+    fn render(&self, &mut Console);
+}
+
+impl Updates for Character {
+    fn update(&mut self, keypress: tcod::KeyState, game: Game) {
         let mut offset = Point { x: 0, y: 0 };
 
         match keypress.key {
-            Special(key_code::Escape) => exit = true,
             Special(key_code::Up) => {
                 offset.y = -1;
             },
@@ -87,23 +98,71 @@ fn main() {
             _ => {}
         }
 
-        match window_bounds.contains(char_point.offset(offset)) {
-            DoesContain    => char_point = char_point.offset(offset),
+        match game.window_bounds.contains(self.position.offset(offset)) {
+            DoesContain    => self.position = self.position.offset(offset),
             DoesNotContain => {}
         }
+    }
 
+    fn render(&self, console: &mut Console) {
+        console.put_char(self.position.x, self.position.y, self.display_char, background_flag::Set);
+    }
+}
+
+impl Updates for NPC {
+    fn update(&mut self, keypress: tcod::KeyState, game: Game) {
         let offset_x = std::rand::task_rng().gen_range(0, 3i) - 1;
-        match window_bounds.contains(dog_point.offset_x(offset_x)) {
-            DoesContain    => dog_point = dog_point.offset_x(offset_x),
+        match game.window_bounds.contains(self.position.offset_x(offset_x)) {
+            DoesContain    => self.position = self.position.offset_x(offset_x),
             DoesNotContain => {}
         }
 
         let offset_y = std::rand::task_rng().gen_range(0, 3i) - 1;
-        match window_bounds.contains(dog_point.offset_y(offset_y)) {
-            DoesContain    => dog_point = dog_point.offset_y(offset_y),
+        match game.window_bounds.contains(self.position.offset_y(offset_y)) {
+            DoesContain    => self.position = self.position.offset_y(offset_y),
             DoesNotContain => {}
         }
+    }
 
-        render(&mut con, char_point, dog_point);
+    fn render(&self, console: &mut Console) {
+        console.put_char(self.position.x, self.position.y, self.display_char, background_flag::Set);
+    }
+}
+
+fn render(con: &mut Console, objs: &Vec<Box<Updates>>) {
+    con.clear();
+    for i in objs.iter() {
+        i.render(con)
+    }
+    con.flush();
+}
+
+fn update(objs: &mut Vec<Box<Updates>>, keypress: tcod::KeyState, game: Game) {
+    for i in objs.mut_iter() {
+        i.update(keypress, game);
+    }
+}
+
+fn main() {
+    let mut game = Game { exit: false, window_bounds: Bound { min: Point { x: 0, y: 0 }, max: Point { x: 79, y: 49 } } };
+    let mut con = Console::init_root(game.window_bounds.max.x + 1, game.window_bounds.max.y + 1, "libtcod Rust tutorial", false);
+
+    let mut c = box Character::new(40i, 25i, '@') as Box<Updates>;
+    let mut d = box NPC::new(10i, 10i, 'd') as Box<Updates>;
+
+    let mut objs: Vec<Box<Updates>> = vec![c, d];
+
+    render(&mut con, &objs);
+
+    while !(Console::window_closed() || game.exit) {
+        let keypress = con.wait_for_keypress(true);
+        match keypress.key {
+            Special(key_code::Escape) => game.exit = true,
+            _                         => {}
+        }
+
+        update(&mut objs, keypress, game);
+
+        render(&mut con, &objs);
     }
 }
