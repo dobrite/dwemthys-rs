aec45370995c25a4c6ce76dae95d8145e2d78839
Dave O <davidobrite@gmail.com>
2014-09-24 06:23:16 -0700

    cleaned up imports

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate tcod;
 extern crate dwemthys;
 
-use dwemthys::util::{Point, Bound};
 use dwemthys::game::Game;
-use dwemthys::traits::{Updates};
+use dwemthys::traits::Updates;
 use dwemthys::character::Character;
 use dwemthys::npc::NPC;
 use dwemthys::rendering::RenderingComponent;
@@ -13,10 +12,10 @@ use tcod::{Console, key_code, Special};
 fn main() {
     let mut game = Game::new();
     let mut c = Character::new(40i32, 25i32, '@');
-    let d  = box NPC::new(10i32, 10i32, 'd') as Box<Updates>;
-    let ct = box NPC::new(40, 25, 'c') as Box<Updates>;
-
-    let mut npcs: Vec<Box<Updates>> = vec![d, ct];
+    let mut npcs: Vec<Box<Updates>> = vec![
+        box NPC::new(10i32, 10i32, 'd') as Box<Updates>,
+        box NPC::new(40i32, 25i32, 'c') as Box<Updates>,
+    ];
 
     game.render(&npcs, c);
 
